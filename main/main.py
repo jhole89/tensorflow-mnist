6ad33fceae32e20804e5a2601917cb3c43cce8ec
jhole89 <joellutman@gmail.com>
2017-03-13 23:14:20 +0000

    tensorflow placeholders

--- main/main.py
+++ main/main.py
@@ -5,3 +5,18 @@ node2 = tf.constant(4.0)
 
 sess = tf.Session()
 print(sess.run([node1, node2]))
+
+node3 = tf.add(node1, node2)
+print("sess.run(node3): ", sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+
+adder_node = a + b
+
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+
+add_and_triple = adder_node * 3
+
+print(sess.run(add_and_triple, {a: 3, b: 4.5}))
